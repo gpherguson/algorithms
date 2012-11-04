22b03c2a4379f7a2f7ac433a3ad90e95f87f6ec7
gpherguson <8si.greg@gmail.com>
2012-11-04 16:36:16 -0700

    added

---
+++ binary_search.rb
@@ -0,0 +1,28 @@
+def binary_search(array, word)
+
+  low, high = 0, array.size - 1
+
+  while (low <= high)
+
+    try = (high - low) / 2
+
+    case
+
+    when (array[try] < word)
+      low = try + 1    
+
+    when (array[try] > word)
+      high = try - 1
+
+    else
+      return try
+
+    end
+    
+  end
+
+  return
+end
+
+array = %w[a ant be bee cat cow do don't egg elephant]
+puts array[binary_search(array, 'bear')]
